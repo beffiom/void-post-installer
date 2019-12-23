1586fdda29c0f22fa47a5e3a326de9437832d583
beffiom <beffiom@protonmail>
2019-12-23 15:24:41 -0500

    changes innit

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ clear
 
 echo "Importing files from server..."
 sleep 3s
-	sudo xpbs-install -Sy git rsync
+	sudo xbps-install -Sy git rsync
 	sudo rm -rvf ~/.*
 	git clone https://github.com/beffiom/dotfiles/
 	rsync -rav ~/dotfiles/.* ~/
@@ -37,19 +37,19 @@ clear
 
 echo "Installing packages..."
 sleep 3s
-	sudo xbps-install -Sy linux5.4 linux5.4-headers xorg-minimal xf86-input-libinput xf86-input-synaptics linux-firmware-intel xf86-video-fbdev xautolock xbacklight xclip xinit xmodmap xscreensaver xwallpaper libva make gcc
-	sudo xbps-install -Sy xf86-video-intel libva-intel-driver
+	sudo xbps-install -Sy xorg-minimal xf86-input-libinput xf86-input-synaptics xf86-video-fbdev xautolock xbacklight xclip xclipboard xinit xmodmap xscreensaver xwallpaper libva make gcc base-devel libX11-devel libXrandr-devel libXft-devel libXinerama-devel pkg-config
+	sudo xbps-install -Sy xf86-video-intel libva-intel-driver linux-firmware-intel
 	sudo xbps-install -Sy alsa-utils pulseaudio alsa-plugins-pulseaudio
 	sudo xbps-install -Sy acpi acpid bash-completion connman dash dunst htop libnotify neovim pulsemixer redshift wpa_supplicant unclutter-xfixes hunspell hunspell-en_US mythes
 	sudo xbps-install -Sy p7zip libzip unzip zip
-	sudo xbps-install -Sy python python-pip python-pyperclip
+	sudo xbps-install -Sy python3 python3-devel python3-pip python3-pyperclip
 	sudo xbps-install -Sy fontconfig nerd-fonts-otf font-awesome breeze-purple-cursor-theme
 	sudo xbps-install -Sy bspwm compton polybar sxhkd
 	sudo xbps-install -Sy qutebrowser
 	sudo xbps-install -Sy ffmpeg ImageMagick maim sxiv
 	sudo xbps-install -Sy ffmpegthumbnailer poppler vifm
 	sudo xbps-install -Sy mpv youtube-dl castero newsboat
-	sudo xbps-install -Sy mpd mpc ncmpccp
+	sudo xbps-install -Sy mpd mpc ncmpcpp
 	sudo xbps-install -Sy zathura zathura-cb zathura-djvu zathura-pdf-mupdf kiwix-tools
 	sudo xbps-install -Sy neofetch cmatrix
 	# sudo xbps-install -Sy gtk+3 plata-theme libreoffice-writer
@@ -59,7 +59,7 @@ sleep 3s
 	sudo pip3 install bs4
 	sudo pip3 install urllib5
 	sudo pip3 install ueberzug
-	sudo pip3 instlal keepmenu
+	sudo pip3 install keepmenu
 
 	sudo xbps-remove -Ry nano
 	sudo xbps-remove -Oo
@@ -69,24 +69,24 @@ clear
 echo "Configuring system..."
 sleep 3s
 
-	sudo ln -s /etc/sv/dbus /var/service/
-	sudo ln -s /etc/sv/alsa /var/service/
-	sudo ln -s /etc/sv/acpid /var/service/
-	sudo ln -s /etc/sv/cgmanager /var/service/
-	sudo ln -s /etc/sv/upower /var/service/
-	sudo ln -s /etc/sv/dhcpcd /var/service/
-	sudo ln -s /etc/sv/pulseaudio /var/service/
-	sudo ln -s /etc/sv/connmand /var/service/
+	sudo ln -sf /etc/sv/dbus /var/service/
+	sudo ln -sf /etc/sv/alsa /var/service/
+	sudo ln -sf /etc/sv/acpid /var/service/
+	sudo ln -sf /etc/sv/cgmanager /var/service/
+	sudo ln -sf /etc/sv/upower /var/service/
+	sudo ln -sf /etc/sv/dhcpcd /var/service/
+	sudo ln -sf /etc/sv/pulseaudio /var/service/
+	sudo ln -sf /etc/sv/connmand /var/service/
 	# sudo ln -s /etc/sv/libvirtd /var/service/
 	# sudo ln -s /etc/sv/virtlogd /var/service/
 	# sudo ln -s /etc/sv/virtlockd /var/service/
-	sudo ln -s /etc/sv/acpid /var/service
+	sudo ln -sf /etc/sv/acpid /var/service
 
-	sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
-	sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
-	sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
-	sudo ln -s /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
-	sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+	sudo ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
+	sudo ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+	sudo ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+	sudo ln -sf /usr/share/fontconfig/conf.avail/50-user.conf /etc/fonts/conf.d/
+	sudo ln -sf /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
 
 	sudo ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 	sudo hwclock --systohc --utc
@@ -114,8 +114,9 @@ sleep 3s
 	sudo usermod -aG input $USER
 	sudo usermod -aG audio $USER
 	sudo usermod -aG video $USER
-	sudo usermod -aG libvirt $USER
-	sudo usermod -aG kvm $USER
+	sudo usermod -aG wheel $USER
+	# sudo usermod -aG libvirt $USER
+	# sudo usermod -aG kvm $USER
 	sudo usermod -aG pulse-access $USER
 
 	sudo xbps-install -Syu

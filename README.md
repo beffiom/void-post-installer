d20900bf23877d8e36390e3168c45e93ceeab385
beffiom <beffiom@protonmail.com>
2020-04-23 15:27:35 -0400

    back to void

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 installs my dotfiles and key programs after a minimal void linux install
 
 ## instructions
-1. `git clone https://github.com/beffiom/void-post-installer`
-2. `cd void-post-installer`
-3. `. install.sh`
+1. login as root
+2. `xbps-install doas nvim`
+3. `nvim /etc/doas.conf`
+4. add this line:    permit :wheel nopass
+5. save and reboot
+6. login user
+7. `git clone https://github.com/beffiom/void-post-installer`
+8. `cd void-post-installer`
+9. `. install.sh`
--- install.sh
+++ install.sh
@@ -40,26 +40,28 @@ sleep 3s
 	doas xbps-install -Sy xorg xf86-input-libinput xf86-input-synaptics xf86-video-fbdev xautolock xbacklight xclip xclipboard xinit xmodmap xscreensaver xwallpaper libva make gcc base-devel libX11-devel libXrandr-devel libXft-devel libXinerama-devel pkg-config
 	doas xbps-install -Sy xf86-video-intel libva-intel-driver linux-firmware-intel
 	doas xbps-install -Sy alsa-utils pulseaudio alsa-plugins-pulseaudio
+	doas xbps-install -Sy bluez bluez-alsa libbluetooth
 	doas xbps-install -Sy acpi acpid bash-completion connman curl dash dunst htop libnotify neovim pulsemixer redshift st-terminfo wget wpa_supplicant unclutter-xfixes hunspell hunspell-en_US mythes
 	doas xbps-install -Sy p7zip libzip unzip zip
 	doas xbps-install -Sy python3 python3-devel python3-pip python3-pyperclip
-	doas xbps-install -Sy fontconfig nerd-fonts-otf font-awesome breeze-purple-cursor-theme
+	doas xbps-install -Sy fontconfig noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf ttf-ubuntu-font-family font-awesome breeze-purple-cursor-theme
 	doas xbps-install -Sy bspwm compton polybar sxhkd
 	doas xbps-install -Sy qutebrowser
 	doas xbps-install -Sy ffmpeg ImageMagick maim sxiv
-	doas xbps-install -Sy ffmpegthumbnailer poppler vifm
+	doas xbps-install -Sy ffmpegthumbnailer ffmpegthumbs poppler vifm
 	doas xbps-install -Sy mpv youtube-dl castero newsboat
-	doas xbps-install -Sy mpd mpc ncmpcpp
+	doas xbps-install -Sy mpd mpc ncmpcpp inotify-tools
 	doas xbps-install -Sy zathura zathura-cb zathura-djvu zathura-pdf-mupdf kiwix-tools
 	doas xbps-install -Sy neofetch cmatrix
-	doas xbps-install -Sy gtk+3 plata-theme libreoffice-writer
-	# doas xbps-install -Sy libvirt qemu virt-manager ebtables dnsmasq
+	doas xbps-install -Sy gtk+3 plata-theme
+	doas xbps-install -Sy libvirt qemu virt-manager ebtables dnsmasq
 	# doas xbps-install -Sy minetest retroarch ppsspp
 
-	pip3 install --user bs4
-	pip3 install --user urllib5
-	pip3 install --user ueberzug
-	pip3 install --user keepmenu
+	pip3 install bs4
+	pip3 install urllib5
+	pip3 install ueberzug
+	pip3 install keepmenu
+	pip3 install castero
 
 	doas xbps-remove -Ry nano
 	doas xbps-remove -Oo
@@ -77,9 +79,9 @@ sleep 3s
 	doas ln -sf /etc/sv/dhcpcd /var/service/
 	doas ln -sf /etc/sv/pulseaudio /var/service/
 	doas ln -sf /etc/sv/connmand /var/service/
-	# doas ln -s /etc/sv/libvirtd /var/service/
-	# doas ln -s /etc/sv/virtlogd /var/service/
-	# doas ln -s /etc/sv/virtlockd /var/service/
+	doas ln -s /etc/sv/libvirtd /var/service/
+	doas ln -s /etc/sv/virtlogd /var/service/
+	doas ln -s /etc/sv/virtlockd /var/service/
 	doas ln -sf /etc/sv/acpid /var/service
 
 	doas ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf /etc/fonts/conf.d/
@@ -115,8 +117,8 @@ sleep 3s
 	doas usermod -aG audio $USER
 	doas usermod -aG video $USER
 	doas usermod -aG wheel $USER
-	# doas usermod -aG libvirt $USER
-	# doas usermod -aG kvm $USER
+	doas usermod -aG libvirt $USER
+	doas usermod -aG kvm $USER
 	doas usermod -aG pulse-access $USER
 
 	doas xbps-install -Syu

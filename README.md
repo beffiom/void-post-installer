8c73043f460d6960cd3e79679c87615f07d11527
beffiom <beffiom@protonmail.com>
2020-12-24 12:18:45 -0500

    back to void

--- README.md
+++ README.md
@@ -3,11 +3,10 @@ installs my dotfiles and key programs after a minimal void linux install
 
 ## instructions
 1. login as root
-2. `xbps-install opendoas neovim`
-3. `nvim /etc/doas.conf`
-4. add this line:    permit :wheel nopass
-5. save and reboot
-6. login user
-7. `git clone https://github.com/beffiom/void-post-installer`
-8. `cd void-post-installer`
-9. `. install.sh`
+2. xbps-install -U
+3. `xbps-install opendoas neovim`
+4. nvim /etc/doas.conf
+5. add "permit nopass :wheel"
+6. `git clone https://github.com/beffiom/void-post-installer`
+7. `cd void-post-installer`
+8. `. install.sh`
--- install.sh
+++ install.sh
@@ -41,21 +41,21 @@ sleep 3s
 	doas xbps-install -Sy xf86-video-intel libva-intel-driver linux-firmware-intel
 	doas xbps-install -Sy alsa-utils pulseaudio alsa-plugins-pulseaudio
 	doas xbps-install -Sy bluez bluez-alsa libbluetooth
-	doas xbps-install -Sy acpi acpid bash-completion connman curl dash dunst htop libnotify neovim pulsemixer redshift st-terminfo wget wpa_supplicant unclutter-xfixes hunspell hunspell-en_US mythes
+	doas xbps-install -Sy acpi acpid bash-completion NetworkManager networkmanager-dmenu curl dash dunst htop libnotify neovim pulsemixer redshift st-terminfo wget wpa_supplicant unclutter-xfixes hunspell hunspell-en_US mythes
 	doas xbps-install -Sy p7zip libzip unzip zip
-	doas xbps-install -Sy python3 python3-devel python3-pip python3-pyperclip
-	doas xbps-install -Sy fontconfig noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf font-symbola ttf-ubuntu-font-family font-awesome breeze-purple-cursor-theme
+	doas xbps-install -Sy python3 python3-dbus python3-devel python3-pip python3-pyperclip
+	doas xbps-install -Sy fontconfig noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf font-symbola ttf-ubuntu-font-family font-awesome breeze-purple-cursor-theme font-hack-ttf
 	doas xbps-install -Sy bspwm compton polybar sxhkd
 	doas xbps-install -Sy qutebrowser
 	doas xbps-install -Sy ffmpeg ImageMagick maim sxiv
-	doas xbps-install -Sy ffmpegthumbnailer ffmpegthumbs poppler vifm pcmanfm
-    doas xbps-install -Sy mpv youtube-dl castero newsboat
+	doas xbps-install -Sy ffmpegthumbnailer ffmpegthumbs poppler vifm
+    doas xbps-install -Sy mpv youtube-dl castero newsboat playerctl mpv-mpris
 	doas xbps-install -Sy mpd mpc ncmpcpp inotify-tools rtorrent
 	doas xbps-install -Sy zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 	doas xbps-install -Sy neofetch cmatrix
-	doas xbps-install -Sy gtk+3 lxappearance numix-themes
+	doas xbps-install -Sy gtk+3 lxappearance Adapta
 	doas xbps-install -Sy libvirt qemu virt-manager ebtables dnsmasq privoxy dnscrypt-proxy
-	# doas xbps-install -Sy minetest retroarch ppsspp openrct2 dolphin-emu xonotic speed-dreams xmoto
+	# doas xbps-install -Sy kodi kodi-addon-inputstream-adaptive kodi-addon-peripheral-joystick minetest retroarch ppsspp openrct2 dolphin-emu xonotic speed-dreams xmoto
 
 	pip3 install bs4
 	pip3 install urllib5
@@ -78,7 +78,7 @@ sleep 3s
 	doas ln -sf /etc/sv/upower /var/service/
 	doas ln -sf /etc/sv/dhcpcd /var/service/
 	doas ln -sf /etc/sv/pulseaudio /var/service/
-	doas ln -sf /etc/sv/connmand /var/service/
+	doas ln -sf /etc/sv/NetworkManager /var/service/
 	doas ln -s /etc/sv/libvirtd /var/service/
 	doas ln -s /etc/sv/virtlogd /var/service/
 	doas ln -s /etc/sv/virtlockd /var/service/

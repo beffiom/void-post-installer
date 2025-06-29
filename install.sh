c34f7882fc2cb26e1b281323b0aee20a9f6e6561
Benjamin Effiom <46904041+beffiom@users.noreply.github.com>
2025-06-29 18:48:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sleep 3s
 	    NetworkManager wpa_supplicant \
 	    dunst libnotify inotify-tools \
 	    p7zip libzip unzip zip \
-	    fontconfig noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf font-awesome5 breeze-purple-cursor-theme nerd-fonts hunspell hunspell-en_US mythes \
+	    fontconfig noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf font-awesome5 breeze-purple-cursor-theme breeze-snow-cursor-theme nerd-fonts hunspell hunspell-en_US mythes \
 	    foot neovim bash-completion atuin ripgrep bat fd zoxide neofetch cmatrix wget curl httrack htop nushell dash yt-dlp gallery-dl udisks2 recutils tofi rbw tldr fzf \
 	    python3 python3-dbus python3-devel python3-pip python3-pyperclip clojure babashka clj-kondo clojure-lsp joker leiningen openjdk-common nodejs \
 	    qutebrowser chromium podman emacs \
